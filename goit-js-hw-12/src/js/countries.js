2a5f24404dff775db2829eaa0c554f6c2501c0e1
Tatiana Rak <raktania3@gmail.com>
2020-10-20 19:04:51 +0300

    fixed css,notification

--- goit-js-hw-12/src/js/countries.js
+++ goit-js-hw-12/src/js/countries.js
@@ -1,8 +1,9 @@
 'use strict';
 import '../styles.css';
 const debounce = require('lodash.debounce');
-const { error } = require('@pnotify/core');
 import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+const { error } = require('@pnotify/core');
 import fetchCountries from './fetchCountries.js';
 import countriesTpl from '../templates/countries.hbs';
 import oneCountriesTpl from '../templates/oneCountry.hbs';
@@ -18,12 +19,11 @@ function countrySearch(event) {
   clearCountriesList();
   const inputValue = event.target.value;
 
+
   fetchCountries(inputValue).then(data => {
     // console.log(data);
     if (data.length > 10) {
-      error({
-        text: 'Too many matches found. Please enter a more specific query!',
-      });
+      error({ text: "Too many matches found. Please enter a more specific query!"});
     } else if (data.length === 1) {
       buildListMarkup(data, oneCountriesTpl);
     } else if (data.length > 1 || data.length <= 10) {
@@ -31,10 +31,10 @@ function countrySearch(event) {
     }
   });
 }
-function buildListMarkup(countryes, template) {
-  const markup = countryes.map(country => template(country));
-  refs.countriesList.insertAdjacentHTML('afterbegin', markup);
+const buildListMarkup=(countryes, template)=> {
+  const markup = countryes.map(country => template(country)).join('');
+  refs.countriesList.insertAdjacentHTML('beforeend', markup);
 }
-function clearCountriesList() {
+const clearCountriesList=()=> {
   refs.countriesList.innerHTML = '';
 }
